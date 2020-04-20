d2c6c08a5242ee66b456347bdb993076d95da6f0
Ameer Hamza <juttameerhamza@gmail.com>
2020-04-20 13:16:24 +0500

    mysql updates congfig

--- database/mysql.js
+++ database/mysql.js
@@ -2,12 +2,12 @@ const { Sequelize } = require('sequelize');
 const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD }  = process.env;
 
 const database = new Sequelize(
-    DB_NAME,
-    DB_USER,
-    DB_PASSWORD,
+    DB_NAME || 'u881161821_mahiara',
+    DB_USER || 'u881161821_mahiara',
+    DB_PASSWORD || 'mahiara',
     { 
         dialect: 'mysql', 
-        host: DB_HOST
+        host: DB_HOST || localhost
     }
 );
 
